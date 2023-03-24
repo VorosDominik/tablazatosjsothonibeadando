99f34c778ad8345c7ea1335cc741d3672e7f66a2
VorosDominik <medoxidhun@gmail.com>
2023-03-24 13:50:09 +0100

    alaphelyzet

---
+++ adat.js
@@ -0,0 +1,37 @@
+export const ADAtlista=[
+    {
+        nev:"Dézi",
+        fajta:"keverek",
+        kor:13
+    },
+
+    {
+        nev:"Dombor",
+        fajta:"foxterier",
+        kor:19
+    },
+
+    {
+        nev:"Dénes",
+        fajta:"buldog",
+        kor:14
+    },
+    {
+        nev:"ricsi",
+        fajta:"ASDleves",
+        kor:15
+    },
+    {
+        nev:"Dsadasd",
+        fajta:"buldog",
+        kor:17
+    },
+    {
+        nev:"Lori",
+        fajta:"keverek",
+        kor:2
+    }
+
+
+
+]
---
+++ main.js
@@ -0,0 +1,43 @@
+const Jellemzok=["Név","Fajta","Kor"]
+
+
+
+$(function(){
+
+    const AR=$("article")
+    osszealit()
+
+
+
+
+})
+
+function osszealit(AR){
+    txt=""
+    txt="<tabel>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    txt="</tabel>"
+    AR.append(txt)
+}
